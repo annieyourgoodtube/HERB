122ea043b00e91744d53a18f8b567bd784fdb63e
AnnieHsu <annieyourgoodtube@gmail.com>
2026-01-28 15:06:23 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { fileURLToPath } from 'url'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // 針對 GitHub Pages 部署，必須設定為專案儲存庫名稱
+  base: '/HERB/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./', import.meta.url))
